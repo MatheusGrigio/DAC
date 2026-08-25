dea1eaf4c00485de2678ad4e5d35a9213a4ef85b
leopamps <leo.pamplona96@hotmail.com>
2026-08-25 20:33:14 -0300

    gerente, saque, deposito

---
+++ frontend/src/app/gerente/gerente.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gerente',
+  imports: [],
+  templateUrl: './gerente.html',
+  styleUrl: './gerente.css',
+})
+export class Gerente {
+
+}
---
+++ frontend/src/app/saque/saque.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saque',
+  imports: [],
+  templateUrl: './saque.html',
+  styleUrl: './saque.css',
+})
+export class Saque {
+
+}
