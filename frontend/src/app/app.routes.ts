2948333bb7995a2cc5ec71d5159fea0be9bda862
leoyuiti <leonardoyuiti@ufpr.br>
2026-09-22 15:52:45 -0300

    feat(cliente): adiciona html do dashboard do cliente

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { authGuard } from './auth/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  
+  {
+    path: 'cliente/dashboard',
+    canActivate: [authGuard],
+    data: { role: 'CLIENTE' },
+    loadComponent: () => 
+      import('./cliente/dashboard/dashboard')
+        .then(m => m.DashboardClienteComponent)
+  },
+  
+  {
+    path: 'gerente/dashboard',
+    canActivate: [authGuard],
+    data: { role: 'GERENTE' },
+    loadComponent: () => 
+      import('./gerente/dashboard/dashboard')
+        .then(m => m.DashboardGerenteComponent)
+  },
+  
+  { path: '**', redirectTo: '/login' }
+];
--- frontend/src/app/gerente/dashboard/dashboard.ts
+++ frontend/src/app/gerente/dashboard/dashboard.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
-  selector: 'app-dashboard',
-  imports: [],
+  selector: 'app-dashboard-cliente',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  styleUrls: ['./dashboard.css']
 })
-export class Dashboard {}
+export class DashboardClienteComponent {
+  private authService = inject(AuthService);
+  
+  get usuario() {
+    return this.authService.usuarioLogado();
+  }
+  
+  logout(): void {
+    this.authService.encerrarSessaoLocal();
+  }
+}
